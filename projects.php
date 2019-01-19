700d4b59b2e0f6c259b0275d62e94e25e1f0f633
Dean Salter <dean.m.salter@gmail.com>
2019-01-19 22:51:54 +0000

    Starting work on PHP

--- projects.php
+++ projects.php
@@ -9,6 +9,15 @@
 
 
 <body>
+<form action="insert.php" method="post"  onsubmit="this.submit(); this.reset(); return false;">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
 <h3>Java Script practising</h3>
 <nav>
 <a href="https://www.deansalter.com/palindrome.html"> Plaindrome </a>
@@ -27,5 +36,8 @@
 <a href="https://ap.www.namecheap.com/dashboard">  Namecheap</a>
 <a href="https://www.ssls.com/user/certificates/activate/5048449">  ssl</a><p>---------------------</p>
 <script src="index.js"></script>
+<iframe width="0" height="0" border="0" name="dummyframe" id="dummyframe">
+
+</iframe>
 </body>
 </html>
