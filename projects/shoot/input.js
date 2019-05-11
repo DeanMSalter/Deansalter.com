979fbf265f2c9d74d5e1808497795fa12a5f6eda
DeanMSalter <dean.m.salter@gmail.com>
2019-05-11 18:43:41 +0100

    started beautfying

--- projects/shoot/input.js
+++ projects/shoot/input.js
@@ -1,49 +1,52 @@
-//Original author:
-//https://github.com/jlongster/canvas-game-bootstrap/blob/a878158f39a91b19725f726675c752683c9e1c08/js/input.js
+// Original author:
+// https://github.com/jlongster/canvas-game-bootstrap/blob/a878158f39a91b19725f726675c752683c9e1c08/js/input.js
 
+(function () {
+  let pressedKeys = {}
 
+  function setKey (event, status) {
+    let code = event.keyCode
+    let key
 
-(function() {
-    var pressedKeys = {};
-
-    function setKey(event, status) {
-        var code = event.keyCode;
-        var key;
-
-        switch(code) {
-        case 32:
-            key = 'SPACE'; break;
-        case 37:
-            key = 'LEFT'; break;
-        case 38:
-            key = 'UP'; break;
-        case 39:
-            key = 'RIGHT'; break;
-        case 40:
-            key = 'DOWN'; break;
-        default:
-            // Convert ASCII codes to letters
-            key = String.fromCharCode(code);
-        }
-
-        pressedKeys[key] = status;
+    switch (code) {
+      case 32:
+        key = 'SPACE'
+        break
+      case 37:
+        key = 'LEFT'
+        break
+      case 38:
+        key = 'UP'
+        break
+      case 39:
+        key = 'RIGHT'
+        break
+      case 40:
+        key = 'DOWN'
+        break
+      default:
+        // Convert ASCII codes to letters
+        key = String.fromCharCode(code)
     }
 
-    document.addEventListener('keydown', function(e) {
-        setKey(e, true);
-    });
+    pressedKeys[key] = status
+  }
 
-    document.addEventListener('keyup', function(e) {
-        setKey(e, false);
-    });
+  document.addEventListener('keydown', function (e) {
+    setKey(e, true)
+  })
 
-    window.addEventListener('blur', function() {
-        pressedKeys = {};
-    });
+  document.addEventListener('keyup', function (e) {
+    setKey(e, false)
+  })
 
-    window.input = {
-        isDown: function(key) {
-            return pressedKeys[key.toUpperCase()];
-        }
-    };
-})();
+  window.addEventListener('blur', function () {
+    pressedKeys = {}
+  })
+
+  window.input = {
+    isDown: function (key) {
+      return pressedKeys[key.toUpperCase()]
+    }
+  }
+})()
