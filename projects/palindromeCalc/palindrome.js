3fe028c9ebbf19a1c264f7642e4957bbe63bac6d
DeanMSalter <dean.m.salter@gmail.com>
2019-05-12 01:54:57 +0100

    web component practice stage 1

--- projects/palindromeCalc/palindrome.js
+++ projects/palindromeCalc/palindrome.js
@@ -1,35 +1,75 @@
-"use strict";
-let input = "";
+'use strict' // eslint-disable-line
+import {
+  element
+} from './util.js'; // eslint-disable-line
 
+export class PalindromeCalc extends HTMLElement { // eslint-disable-line
+  constructor() {
+    super()
 
-document.getElementById('submitButton').addEventListener('click', function(){
- console.log("Initial value= " + document.getElementById("inputBox").value)
- input = document.getElementById("inputBox").value;
- calculate(input);
+    const shadow = this.attachShadow({
+      mode: 'open'
+    })
+    element(shadow, 'link', {
+      rel: 'stylesheet',
+      type: "text/css",
+      href: './palindromeCalc/palindrome.css'
+    })
+    console.log(window.location.pathname)
 
-});
-document.getElementById("inputBox")
-    .addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-        document.getElementById("submitButton").click();
+    const container = element(shadow, "div", {
+      id: "palindrome-container"
+    })
+
+    let submitButton = element(container, "button", {
+      id: "submitButton"
+    })
+    let inputBox = element(container, "input", {
+      id: "inputBox"
+    })
+    let title = element(container, "h1", {
+      id: "titleText"
+    })
+    let result = element(container, "h2", {
+      id: "resultText"
+    })
+    let reversedOutput = element(container, "h2", {
+      id: "reversedText"
+    })
+    let summary = element(container, "h3", {
+      id: "summaryText"
+    })
+
+    title.textContent = "Palindrome calculator"
+    submitButton.textContent = "Submit"
+    summary.textContent= "Welcome to my palindrome calculator.\nThis is just a simple application to test if a given string is a palindrome.\nIt was created so I could familarise myself with using grid layouts and web components "
+    let input = ''
+    submitButton.addEventListener('click', function() {
+      input = inputBox.value
+      calculate(input)
+    })
+
+    inputBox.addEventListener("keyup", function(event) {
+      event.preventDefault()
+      if (event.keyCode === 13) {
+        submitButton.click()
+      }
+    })
+
+    function calculate(input) {
+      let reversed = ""
+      for (let i = input.length - 1; i >= 0; i--) {
+        reversed += input[i]
+      }
+      reversed = reversed.toLowerCase()
+      input = input.toLowerCase()
+      if (reversed === input) {
+        result.innerHTML = input + " is a Palindrome!"
+      } else {
+        result.innerHTML = input + " is not a Palindrome!"
+      }
+      reversedOutput.innerHTML = "Reversed = " + reversed
     }
-});
-
-function calculate( input){
-  let reversed = ""
-  for(let i = input.length-1;i>=0;i--){
-    reversed += input[i];
   }
-  reversed = reversed.toLowerCase();
-  input = input.toLowerCase();
-  console.log("Reversed= " + reversed);
-  if(reversed === input){
-    console.log("Palindrome!");
-    document.getElementById('result').innerHTML = input + " is a Palindrome!";
-  }else{
-    console.log("not a Palindrome!");
-    document.getElementById('result').innerHTML = input + " is not a Palindrome!";
-  }
-  document.getElementById('reversed').innerHTML = "Reversed = " + reversed;
 }
+customElements.define('palindrome-calc', PalindromeCalc) // eslint-disable-line
---
+++ projects/palindromeCalc/util.js
@@ -0,0 +1,12 @@
+export function element(parent,eleName,attrs){ // eslint-disable-line
+  const e = document.createElement(eleName)
+  if (attrs == null) {
+    parent.appendChild(e)
+    return e
+  }
+  for (let i=0; i<Object.keys(attrs).length; i++) {
+    e.setAttributeNS(null, Object.keys(attrs)[i], attrs[Object.keys(attrs)[i]])
+  }
+  parent.appendChild(e)
+  return e
+}
