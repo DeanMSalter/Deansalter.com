6e805e198cde954b7aae6a5dafcaca31ca7fbbdf
DeanMSalter <dean.m.salter@gmail.com>
2019-06-29 12:10:40 +0100

    fixed mobile support + added better validation

--- projects/MMS/MMS.js
+++ projects/MMS/MMS.js
@@ -10,8 +10,7 @@ for (let row of rows) {
 
 
     cell.addEventListener("click", function() {
-      if (cell.innerHTML === '<input type="number" class="cellInputs">') { return } //If cell currently has an input box
-
+      if (cell.getElementsByTagName("input")[0]) { return } //If cell currently has an input box
       //
       let oldValue = ""
       if (cell.innerHTML !== "&nbsp;") { //if cell has a saved value
@@ -23,23 +22,49 @@ for (let row of rows) {
       cell.getElementsByTagName("input")[0].focus()
       cell.getElementsByTagName("input")[0].value = oldValue
 
-
       cell.getElementsByTagName("input")[0].addEventListener("keypress", function(e) {
-        if(e.key === "." && (cell.getElementsByTagName("input")[0].value.includes(".") || cell.getElementsByTagName("input")[0].value === "" )) {
-          e.preventDefault()
-          return false
-        }
-        if (isNaN(e.key) && e.key !==".") {
-          console.log(cell.getElementsByTagName("input")[0].value.includes("."))
-          e.preventDefault()
-          return false
-        }
         if (e.keyCode === 13) {
           cell.innerHTML=cell.getElementsByTagName("input")[0].value
+          e.preventDefault()
+          return true
         }
+      })
+      cell.getElementsByTagName("input")[0].addEventListener("input", function(e) {
+        console.log(e)
+        let cellValue = cell.getElementsByTagName("input")[0].value
+        if (e.data === "." && (cellValue.split('.').length-1 > 1 || cellValue === ".")) {
+          console.log("stop")
+          cell.getElementsByTagName("input")[0].value = (cellValue).substring(0, cellValue.length - e.data.length)
+        }
+        if (isNaN(e.data) && e.data !==".") {
+          console.log("Stop")
+          cell.getElementsByTagName("input")[0].value = (cellValue).substring(0, cellValue.length - e.data.length)
+        }
+
+        //store value inputted into the actual cell
+      })
+      cell.getElementsByTagName("input")[0].addEventListener("paste", function(e) {
+   //      clipboardData = e.clipboardData || window.clipboardData;
+   // pastedData = clipboardData.getData('Text');
+
+        let cellValue = cell.getElementsByTagName("input")[0].value
+        if (cellValue !== "") {
+          e.preventDefault()
+          return false
+        }
+        if (e.clipboardData.getData('text') === "." && (cellValue.split('.').length-1 > 1 || cellValue === ".")) {
+          e.preventDefault()
+          return false
+        }
+        if (isNaN(e.clipboardData.getData('text')) && e.clipboardData.getData('text') !==".") {
+          e.preventDefault()
+          return false
+        }
+
         //store value inputted into the actual cell
       })
       cell.getElementsByTagName("input")[0].addEventListener("focusout", function() {
+        console.log(document.activeElement)
         cell.innerHTML=cell.getElementsByTagName("input")[0].value
       })
     })
