3b305eacbb2d39fab4b0699a1a7dcfcd12fdd0d3
= <dean.m.salter@gmail.com>
2020-04-24 17:52:33 +0100

    tidied up back and front end

--- notes/insertNote.php
+++ notes/insertNote.php
@@ -1,42 +1,80 @@
 <?php
 include ('../main.php');
 
-$mysqli = mysqliConnect();
 $noteTitle = "$_POST[noteTitle]";
 $noteContent = "$_POST[noteContent]";
 $idToken = "$_POST[idToken]";
+$noteId = isset($_POST['noteId']) ? $_POST['noteId'] : null;
 
-$stmt = $mysqli->prepare("INSERT INTO note (noteTitle, noteContent) VALUES (?,?)");
-$stmt->bind_param('ss', $noteTitle, $noteContent);
-$stmt->execute();
-$stmt->close();
-$noteId = $mysqli->insert_id;
-$mysqli->close();
+try{
+    $mysqli = mysqliConnect();
+    if($noteId){
+        if(validUserForNote($mysqli, $noteId, $idToken)){
+            $editNoteContentStmt = $mysqli->prepare("UPDATE note set noteContent = ? where noteId = ?");
+            $editNoteContentStmt->bind_param('ss', $noteContent, $noteId);
+            $editNoteContentStmt->execute();
+            $editNoteContentStmt->close();
 
-$user = getUserFromTokenId($idToken);
-$mysqli2 = mysqliConnect();
+            $editNoteTitleStmt = $mysqli->prepare("UPDATE note set noteTitle = ? where noteId = ?");
+            $editNoteTitleStmt->bind_param('ss', $noteTitle, $noteId);
+            $editNoteTitleStmt->execute();
+            $editNoteTitleStmt->close();
+            $mysqli->close();
+            echo json_encode(array(
+                'successful' => array(
+                    'noteId' => $noteId,
+                    'noteContent' => $noteContent,
+                    'noteTitle' => $noteTitle,
+                ),
+            ));
+        }else{
+            $mysqli->close();
+            throw new RuntimeException('Given UserId does not match the notes UserId', 401);
+        }
+    }else{
+        $insertNoteStmt = $mysqli->prepare("INSERT INTO note (noteTitle, noteContent) VALUES (?,?)");
+        $insertNoteStmt->bind_param('ss', $noteTitle, $noteContent);
+        $insertNoteStmt->execute();
+        $insertNoteStmt->close();
+        $noteId = $mysqli->insert_id;
+        $mysqli->close();
 
-if ($user) {
-    $userId = $user['sub'];
-    $firstName = $user['given_name'];
-    $lastName = $user['family_name'];
-    $email = $user['email'];
-    $validUser = findUser($mysqli2, $userId);
-    if(empty($validUser)){
-        insertUser($mysqli2, $userId, $firstName, $lastName, $email);
+        $user = getUserFromTokenId($idToken);
+        $mysqli2 = mysqliConnect();
+
+        if ($user) {
+            $userId = $user['sub'];
+            $firstName = $user['given_name'];
+            $lastName = $user['family_name'];
+            $email = $user['email'];
+            $validUser = findUser($mysqli2, $userId);
+            if(empty($validUser)){
+                insertUser($mysqli2, $userId, $firstName, $lastName, $email);
+            }
+
+            $insertUserNoteStmt = $mysqli2->prepare("INSERT INTO userNote (userId, noteId) VALUES (?,?)");
+            $insertUserNoteStmt->bind_param('ss', $userId, $noteId);
+            $insertUserNoteStmt->execute();
+            $insertUserNoteStmt->close();
+            $mysqli2->close();
+            echo json_encode(array(
+                'successful' => array(
+                    'noteId' => $noteId,
+                    'noteContent' => $noteContent,
+                    'noteTitle' => $noteTitle,
+                ),
+            ));
+        } else {
+            $mysqli2->close();
+            throw new RuntimeException('Invalid TokenId Given', 404);
+        }
     }
-
-    echo "inserting into userNote";
-    $stmt2 = $mysqli2->prepare("INSERT INTO userNote (userId, noteId) VALUES (?,?)");
-    $stmt2->bind_param('ss', $userId, $noteId);
-    $stmt2->execute();
-    printf("Error: %s.\n", $stmt2->error);
-
-    $stmt2->close();
-} else {
-    echo "invalid token id";
+}catch(Exception $e){
+    echo json_encode(array(
+        'error' => array(
+            'msg' => $e->getMessage(),
+            'code' => $e->getCode(),
+        ),
+    ));
 }
-$mysqli2->close();
-
-
 ?>
--- notes/notes.js
+++ notes/notes.js
@@ -5,80 +5,112 @@ $(function () {
 });
 
 $(document).ready(function () {
-    resultsTable = $('#noteListTable').DataTable({
-             "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ]
-        });
-
-    CKEDITOR.replace( 'noteContentArea' );
-    $("#submitNote").on("click",function () {
-        let noteId = $("#noteId").val();
-        if(noteId){
-            $.ajax({
-                url:"editNote.php ",
-                method:"POST",
-                data:{
-                    noteTitle: $("#noteTitle").val(), // Second add quotes on the value.
-                    noteContent: CKEDITOR.instances['noteContentArea'].getData(),
-                    noteId: noteId,
-                    idToken: localStorage.getItem("idToken"),
-
-                },
-                success:function(response) {
-                    loadNotes();
-                },
-                error:function(){
-                    //TODO: better error message
-                    alert("error");
-                }
-            });
-        }else{
-            $.ajax({
-                url:"insertNote.php ",
-                method:"POST",
-                data:{
-                    noteTitle: $("#noteTitle").val(), // Second add quotes on the value.
-                    noteContent: CKEDITOR.instances['noteContentArea'].getData(),
-                    idToken: localStorage.getItem('idToken')
-                },
-                success:function(response) {
-                    CKEDITOR.instances['noteContentArea'].setData('');
-                    $("#noteTitle").val("");
-                    loadNotes();
-                },
-                error:function(){
-                    //TODO: better error message
-                    alert("error");
-                }
-
-            });
-        }
+    resultsTable = $("#noteListTable").DataTable({
+        "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
     });
-    $("#newNoteToggle").on("click",function () {
-        let noteEntry = $("#noteEntry")
 
-        if(noteEntry.css("display") === "none"){
-            noteEntry.css("display","block");
-            $(this).attr("value","Hide");
-        }else{
-            $(this).attr("value","New Note");
-            noteEntry.css("display","none");
-        }
-    });
+    CKEDITOR.replace('noteContentArea');
+    $("#submitNote").on("click",submitNote);
+    $("#newNoteToggle").on("click",toggleNewNote);
     loadNotes()
 });
 
+function submitNote(){
+    let noteId = $("#noteId").val();
+    let noteTitle = $("#noteTitle").val();
+    let noteContent = CKEDITOR.instances['noteContentArea'].getData();
+    let idToken = localStorage.getItem('idToken');
+
+    $.ajax({
+        url:"insertNote.php ",
+        method:"POST",
+        data:{
+            noteTitle: noteTitle,
+            noteContent: noteContent,
+            idToken: idToken,
+            noteId: noteId
+        },
+        success:function(response) {
+            response = JSON.parse(response);
+            if(response.error){
+                alert(response.error["msg"])
+            }else{
+                if(!noteId){
+                    clearNoteEntry();
+                }
+                loadNotes();
+            }
+        }
+    });
+}
+
+
+function removeNoteListeners(){
+    $( "input[name^='removeNote_']" ).on("click",function () {
+        let selectedRow = $(this).closest("tr")
+        let noteId = $(this).attr("name").split("_")[1]
+        $.ajax({
+            url:"changeNoteStatus.php ",
+            method:"POST",
+            data:{
+                noteId: noteId,
+                idToken: localStorage.getItem("idToken"),
+                noteStatus: "NOTE_REMOVED",
+            },
+            success:function(response) {
+                response = JSON.parse(response);
+                if(response.error){
+                    alert(response.error["msg"])
+                }else{
+                    resultsTable.row(selectedRow).remove().draw();
+                    if(noteId === $("#noteId").val()){
+                        clearNoteEntry();
+                    }
+                }
+            },
+        });
+    })
+}
+
+function editNoteListeners(){
+    $( "input[name^='editNote_']" ).on("click",function () {
+        let noteId = $(this).attr("name").split("_")[1];
+        $.ajax({
+            url:"loadNote.php ",
+            method:"POST",
+            data:{
+                noteId: noteId,
+                idToken: localStorage.getItem("idToken"),
+            },
+            success:function(response) {
+                response = JSON.parse(response);
+                if(response.error){
+                    alert(response.error["msg"])
+                }else{
+                    let note = response.note;
+                    CKEDITOR.instances['noteContentArea'].setData(note["noteContent"]);
+                    $("#noteTitle").val(note["noteTitle"]);
+                    $("#noteId").val(note["noteId"]);
+                    $("#noteEntry").css("display","block");
+                    $("#newNoteToggle").attr("value","Hide");
+                }
+            }
+        });
+    })
+}
+
 function loadNotes(){
+    let currentUserId = localStorage.getItem("userId");
     $.ajax({
         url:"loadNotes.php ",
         method:"GET",
-
         success:function(response) {
             resultsTable.clear();
             let responseParsed = JSON.parse(response);
             for(let row = 0; row<responseParsed.length ; row++) {
                 let removeNote = "";
                 let editNote = "";
-                if(localStorage.getItem("userId") === responseParsed[row].userId || !responseParsed[row].userId){
+                if(currentUserId === responseParsed[row].userId || !responseParsed[row].userId){
                     removeNote = "<input type=\'button\' name=\'removeNote_" +  responseParsed[row].noteId + "\' value=\'Remove Note\'>";
                     editNote = "<input type=\'button\' name=\'editNote_" +  responseParsed[row].noteId + "\' value=\'Edit Note\'>";
                 }
@@ -91,76 +123,33 @@ function loadNotes(){
                 resultsTable.row.add(newRow)
             }
             resultsTable.draw();
-            $( "input[name^='removeNote_']" ).on("click",function () {
-                let selectedRow = $(this).closest("tr")
-                let noteId = $(this).attr("name").split("_")[1]
-                $.ajax({
-                    url:"changeNoteStatus.php ",
-                    method:"POST",
-                    data:{
-                        noteId: noteId,
-                        idToken: localStorage.getItem("idToken"),
-                        noteStatus: "NOTE_REMOVED",
-                    },
-                    success:function(response) {
-                        response = JSON.parse(response);
-                        if(response.error){
-                            alert(response.error["msg"])
-                        }else{
-                            resultsTable.row(selectedRow).remove().draw();
-                        }
-                    },
-                    error:function(){
-                        //TODO: better error message
-                        alert("error");
-                    }
-
-                });
-            })
-            $( "input[name^='editNote_']" ).on("click",function () {
-                let noteId = $(this).attr("name").split("_")[1];
-                $.ajax({
-                    url:"loadNote.php ",
-                    method:"POST",
-                    data:{
-                        noteId: noteId,
-                        idToken: localStorage.getItem("idToken"),
-                    },
-                    success:function(response) {
-                        response = JSON.parse(response);
-                        console.log(response);
-                        if(response.error){
-                            alert(response.error["msg"])
-                        }else{
-                            let note = response.note;
-                            CKEDITOR.instances['noteContentArea'].setData(note["noteContent"]);
-                            $("#noteTitle").val(note["noteTitle"]);
-                            $("#noteId").val(note["noteId"]);
-                            let noteEntry = $("#noteEntry");
-                            noteEntry.css("display","block");
-                            $("#newNoteToggle").attr("value","Hide");
-                        }
-
-                    },
-                    error:function(){
-                        //TODO: better error message
-                        alert("error");
-                    }
-                });
-            })
-
+            removeNoteListeners();
+            editNoteListeners();
         },
-        error:function(){
-            //TODO: better error message
-            alert("error");
-        }
-
     });
+}
+
+function toggleNewNote(){
+    let noteEntry = $("#noteEntry");
+    if(noteEntry.css("display") === "none"){
+        noteEntry.css("display","block");
+        $(this).attr("value","Hide");
+    }else{
+        $(this).attr("value","New Note");
+        noteEntry.css("display","none");
+    }
 }
 
 function signedOut(){
     loadNotes();
 }
+
 function signedIn(){
     loadNotes();
+}
+
+function clearNoteEntry(){
+    CKEDITOR.instances['noteContentArea'].setData('');
+    $("#noteTitle").val("");
+    $("#noteId").val("");
 }
